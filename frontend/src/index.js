b14ed76ca979af0a05712b4b4aa92a3f72b708ee
Newton <57525074+lilixac@users.noreply.github.com>
2022-01-24 08:11:08 +0545

    Notification styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'react-notifications/lib/notifications.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
